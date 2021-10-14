e7a76f42591cf5e98057da97d9985d3c2cad36e7
Hauliu <13607047104@163.com>
2021-10-14 21:34:32 +0800

    添加新组件

--- day03_cil/src/main.js
+++ day03_cil/src/main.js
@@ -1,8 +1,9 @@
 import Vue from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
+import Test from './test.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  render: h => h(Test),
 }).$mount('#app')
---
+++ day03_cil/src/test.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h4>
+      <p :title="time()">{{ name }}</p>
+    </h4>
+  </div>
+</template>
+
+<style>
+    .test-box {
+        background-color: pink;
+    }
+</style>
+<script>
+export default {
+ data() {
+     return{
+         name: 'iiiiiii',
+          
+     }
+ },
+ methods: {
+     time(){
+         console.log()
+         const time = new Date().toLocaleString()
+         return time
+     }
+ },
+};
+</script>
+
+
