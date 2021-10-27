67a67d3ff1137b4f1db76f17a465c164014edb75
Hauliu <13607047104@163.com>
2021-10-27 10:03:00 +0800

    ‘新增demo文件’

---
+++ day_02/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app-container">
+    <h1>App 根组件</h1>
+    <hr />
+
+    <div class="box">
+      <!-- 渲染 Left 组件和 Right 组件 -->
+      <Left :msg="message" :user="userinfo"></Left>
+    </div>
+  </div>
+</template>
+
+<script>
+import Left from "@/components/Left.vue";
+export default {
+  data() {
+    return {
+      message: "hello 132 宝宝们",
+      userinfo: { name: "wsc", age: 18 },
+    };
+  },
+  components: {
+    Left,
+  },
+};
+</script>
+
+<style lang="less">
+.app-container {
+  padding: 1px 20px 20px;
+  background-color: #efefef;
+}
+.box {
+  display: flex;
+}
+</style>
